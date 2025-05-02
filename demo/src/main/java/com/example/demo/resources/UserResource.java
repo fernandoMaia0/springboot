0e64373c872bc361375d449c9d73290d54d79a74
fernandoMaia0 <fernandomaia.dev@gmail.com>
2025-05-02 20:16:39 -0300

    getting user by id

--- demo/src/main/java/com/example/demo/resources/UserResource.java
+++ demo/src/main/java/com/example/demo/resources/UserResource.java
@@ -2,11 +2,13 @@ package com.example.demo.resources;
 
 import java.util.List;
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,17 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(listDto);
     }
+    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findByID(@PathVariable String id){
+        User obj = service.findByID(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+    
+
 }
--- demo/src/main/java/com/example/demo/services/UserService.java
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findByID(String id){
+        User user = repo.findById(id).orElse(null);
+        if(user == null){
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return user;
     }
 }
