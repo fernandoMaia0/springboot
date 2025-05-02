61806e08812ac7a3fd19b775461763fb9ce06d03
fernandoMaia0 <fernandomaia.dev@gmail.com>
2025-05-02 20:51:58 -0300

    insert user with post

--- demo/src/main/java/com/example/demo/services/UserService.java
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
             throw new ObjectNotFoundException("Object not found");
         }
         return user;
+    }
+
+    public User insert (User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO obj){
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
     }
 }
