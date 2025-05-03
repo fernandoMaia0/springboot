d4e81f37e1da7154909bbb0d00b4a8bf06786457
fernandoMaia0 <fernandomaia.dev@gmail.com>
2025-05-03 13:24:32 -0300

    Entity Post and RepositoryPost

--- demo/src/main/java/com/example/demo/config/Instantiation.java
+++ demo/src/main/java/com/example/demo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.example.demo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
+import com.example.demo.dto.AuthorDTO;
+import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +20,28 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+    
     @Override
     public void run(String... args) throws Exception {
         
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Fernando Maia", "fernando@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
+        
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2025"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2025"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        postRepository.saveAll(Arrays.asList(post1,post2));
 
     }
 
