1030c0a76a783e7e952f1e82e39ac5159eba3e42
fernandoMaia0 <fernandomaia.dev@gmail.com>
2025-05-03 16:49:12 -0300

    implementation fullSearch post

--- demo/src/main/java/com/example/demo/repository/PostRepository.java
+++ demo/src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Post;
@@ -11,5 +13,16 @@ import com.example.demo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
     
     List<Post> findByTitleContainingIgnoreCase(String text);
+    
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, "+
+            "{ $or: [ "+ //inicio or 
+            "{ 'title': { $regex: ?0, $options: 'i' } } , "+
+            "{ 'body': { $regex: ?0, $options: 'i' } }, "+
+            "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] }"+ //final or
+            "] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
